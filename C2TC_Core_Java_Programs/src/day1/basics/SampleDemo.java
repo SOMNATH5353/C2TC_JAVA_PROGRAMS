cd33717d89aeacb8a28e76510b13f24db0bf89a4
Somnath <somnathuma71@gmail.com>
2025-07-21 13:25:08 +0530

    This is the first code

--- C2TC_Core_Java_Programs/src/day1/basics/SampleDemo.java
+++ C2TC_Core_Java_Programs/src/day1/basics/SampleDemo.java
@@ -5,6 +5,8 @@ public class SampleDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello JFS...");
+		System.out.println("Mysel Somnath");
+		System.out.println("Hello Frens");
 
 	}
 
