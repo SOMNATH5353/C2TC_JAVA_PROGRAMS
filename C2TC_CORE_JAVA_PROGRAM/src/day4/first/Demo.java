5b8c577a5b1ede0ba27c6a895e88c16917c1c91d
Somnath <somnathuma71@gmail.com>
2025-07-24 14:42:24 +0530

    ....

--- C2TC_CORE_JAVA_PROGRAM/src/day4/first/Demo.java
+++ C2TC_CORE_JAVA_PROGRAM/src/day4/first/Demo.java
@@ -1,11 +1,10 @@
-package dayfour.first;
+package day4.first;
 
 public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello");
-
+System.out.println("hi");
 	}
 
 }
